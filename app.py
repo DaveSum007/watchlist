08f09a360a827d283b002a4aae6cb37ac66edaf1
Dave Sum <sundawei@ruc.edu.cn>
2023-11-27 15:13:07 +0800

    Create,edit and delete item by form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import url_for
+from flask import url_for,request,redirect,flash
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -18,7 +18,7 @@ else:
 
 app.config['SQLALCHEMY_DATABASE_URI']=prefix+os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #关闭对模型修改的监控
-
+app.config['SECRET_KEY']='dev'
 #实例一个数据库
 db=SQLAlchemy(app)
 
@@ -48,12 +48,57 @@ def inject_user():
     user=User.query.first()
     return dict(user=user)
 
-@app.route('/')
+#主页面,面对不同的请求，使用不同的返回方式
+@app.route('/',methods=['GET','POST'])
 def index():
+    if request.method=='POST':
+        #获取表单数据
+        title=request.form.get('title')
+        year=request.form.get('year')
+        #验证数据
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input.')#显示错误提示
+            return redirect(url_for('index'))#重定向回主页
+
+        #保存表单数据到数据库
+        movie=Movie(title=title,year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
+    user=User.query.first()
     movies=Movie.query.all()
-    return render_template('index.html',movies=movies)
+    return render_template('index.html',user=user,movies=movies)
 
+#编辑电影条目
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie=Movie.query.get_or_404(movie_id)
 
+    if request.method=="POST":
+        title=request.form['title']
+        year=request.form['year']
+
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input.')
+            return redirect(url_for('edit',movie_id=movie_id))
+
+        movie.title=title
+        movie.year=year
+        db.session.commit()
+        flash('Item update.')
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie=movie)
+
+#删除电影条目
+@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+def delete(movie_id):
+    movie=Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
 #创建自定义命令forge，该命令能够创建数据库
 @app.cli.command()
 def forge():
